24d4172503588cc6ba2136d3d357e9fc68d744cf
marianitadn <maria.nita.dn@gmail.com>
2012-08-07 09:37:02 +0300

    Second commit, with add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorApp::Application.config.secret_token = 'dd3b49a695d6ed71adf98c75031caa9c1b4c7394ee132dce90afcc1652892460ec5b4837708c78b77be7c6a8227891dc964bf06660665d928373778144eb5365'
